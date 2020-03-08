31e4fd1491a6100245ff9d21aaf551cd7e162956
Arianna Vespri <36129782+vesari@users.noreply.github.com>
2020-03-08 21:34:20 +0100

    Update server/plugin.go
    Co-Authored-By: Michael Kochell <mjkochell@gmail.com>

--- server/plugin.go
+++ server/plugin.go
@@ -78,7 +78,7 @@ func (p *Plugin) OnActivate() error {
 		return errors.Wrap(err, "failed to read profile image")
 	}
 	if appErr = p.API.SetProfileImage(botID, profileImage); appErr != nil {
-		return errors.Wrap(err, "failed to set profile image")
+		return errors.Wrap(appErr, "failed to set profile image")
 	}
 
 	channel, appErr := p.API.GetChannelByName(team.Id, channelSplit, false)
